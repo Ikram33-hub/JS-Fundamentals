c4d06ffee5323f1cce43a64b389d60aa1c8a4a77
Ikram33-hub <az.ikramel@gmail.com>
2025-09-14 22:09:43 +0100

    Add 8-square.js to print square of X of given size

---
+++ 8-square.js
@@ -0,0 +1,14 @@
+const args = process.argv.slice(2);
+const size = parseInt(args[0]);
+
+if (!args[0] || isNaN(size) || size < 1) {
+    console.log("Missing size");
+} else {
+    for (let i = 0; i < size; i++) {
+        let line = "";
+        for (let j = 0; j < size; j++) {
+            line += "X";
+        }
+        console.log(line);
+    }
+}
