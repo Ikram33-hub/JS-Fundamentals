62a2c450a00566359d8bb27a16264cf9ec40e9ad
Ikram33-hub <az.ikramel@gmail.com>
2025-09-14 22:06:20 +0100

    Final fix 7-multi_c.js: use only two console.log and loop

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -8,5 +8,5 @@ if (!args[0] || isNaN(x)) {
     for (let i = 0; i < (x > 0 ? x : -x); i++) {
         output += "C is fun\n";
     }
-    console.log(output.slice(0, -1)); // نحيد آخر \n
+    console.log(output.slice(0, -1)); 
 }
