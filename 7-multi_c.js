2fbc68c6b92908a6f0e5261055cbd9a81cf38b96
Ikram33-hub <az.ikramel@gmail.com>
2025-09-14 22:04:13 +0100

    Final fix 7-multi_c.js: use only two console.log and loop

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -4,7 +4,9 @@ const x = parseInt(args[0]);
 if (!args[0] || isNaN(x)) {
     console.log("Missing number of occurrences");
 } else {
+    let output = "";
     for (let i = 0; i < (x > 0 ? x : -x); i++) {
-        console.log("C is fun");
+        output += "C is fun\n";
     }
+    console.log(output.slice(0, -1)); // نحيد آخر \n
 }
