9efb59bec1b55933e79c0cbd3656943cd69b537a
Ikram33-hub <az.ikramel@gmail.com>
2025-09-14 22:00:02 +0100

    Fix 7-multi_c.js to correctly print 'C is fun' x times for positive and negative numbers

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,10 @@
 const args = process.argv.slice(2);
 const x = parseInt(args[0]);
 
-if (!args[0] || isNaN(x)) { 
+if (!args[0] || isNaN(x)) {
     console.log("Missing number of occurrences");
 } else {
-    for (let i = 0; i < Math.abs(x); i++) { 
+    for (let i = 0; i < (x > 0 ? x : -x); i++) {
         console.log("C is fun");
     }
 }
